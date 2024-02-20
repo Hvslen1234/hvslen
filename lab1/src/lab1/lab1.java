9d24a52035d17660d3d02d368c80692bcdc9ef6f
hvslen <hvslen@172.16.153.146>
2024-02-20 19:04:32 +0800

    initial commit.

---
+++ lab1/src/lab1/lab1.java
@@ -0,0 +1,30 @@
+package lab1;
+
+public class lab1 {
+    private double radius;
+
+    public lab1(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public double calculateCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public static void main(String[] args) {
+        double radius = 5.0; 
+        lab1 circle = new lab1(radius);
+
+        System.out.println("Radius: " + circle.getRadius());
+        System.out.println("Area: " + circle.calculateArea());
+        System.out.println("Circumference: " + circle.calculateCircumference());
+    }
+}
