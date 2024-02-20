85bda7a199093478daf3293111ddeb5b33227433
hvslen <hvslen@172.16.153.146>
2024-02-20 19:45:17 +0800

    inial

---
+++ lab1/src/lab1/GraphExample.java
@@ -0,0 +1,44 @@
+package lab1;
+
+import java.util.*;
+
+class Graph {
+    private Map<Integer, List<Integer>> adjacencyList;
+
+    public Graph() {
+        this.adjacencyList = new HashMap<>();
+    }
+
+    public void addVertex(int vertex) {
+        adjacencyList.put(vertex, new ArrayList<>());
+    }
+
+    public void addEdge(int source, int destination) {
+        adjacencyList.get(source).add(destination);
+        adjacencyList.get(destination).add(source);
+    }
+
+    public void printGraph() {
+        for (int vertex : adjacencyList.keySet()) {
+            System.out.print("Vertex " + vertex + " is connected to: ");
+            for (int neighbor : adjacencyList.get(vertex)) {
+                System.out.print(neighbor + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+public class GraphExample {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+
+        graph.printGraph();
+    }
+}
